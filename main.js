7779180b57b887dbcc47ba7fcbc0fbfefe3fb464
mickeysrishti <Mukeshpathak345@gmail.com>
2024-05-25 14:10:12 +0530

    Code added

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
-document.getElementById("menu-icon").addEventListener("click", function() {
+function toggleMenu() {
     var menu = document.getElementById("mobile-menu");
     if (menu.style.display === "block") {
         menu.style.display = "none";
     } else {
         menu.style.display = "block";
     }
+}
+
+document.getElementById("menu-icon").addEventListener("click", function() {
+    toggleMenu();
+});
+
+document.getElementById("close").addEventListener("click", function() {
+    toggleMenu();
 });
